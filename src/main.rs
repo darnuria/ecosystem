6c6852a64cc61827de797a51dc6659922d206885
Axel Viala <axel.viala@darnuria.eu>
2022-09-12 22:35:15 +0200

    Almost done, some bugs are still here.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ use std::io::Write;
 
 use rand::Rng;
 
-const NUMBER_OF_POOR_ANIMALS: usize = 20;
-const NUMBER_OF_PREDATORS: usize = 20;
+const NUMBER_OF_POOR_ANIMALS: usize = 15;
+const NUMBER_OF_PREDATORS: usize = 15;
 // /const TIME_TO_WAIT: usize = 40000;
-const ITERATIONS: usize = 15;
-const SIZE_X: usize = 20;
-const SIZE_Y: usize = 20;
-const PROBABILITY_MOVE: u32 = 100;
+const ITERATIONS: usize = 2500;
+const SIZE_X: usize = 42;
+const SIZE_Y: usize = 42;
+const PROBABILITY_MOVE: u32 = 25;
 const ENERGY_DEFAULT: i32 = 50;
-const ENERGY_DEFAULT_SPENT_HERBIVOROUS: i32 = 1;
-const REPRODUCTION_RATE_HERBIVOROUS: u32 = 9;
+const ENERGY_DEFAULT_SPENT_HERBIVOROUS: i32 = 2;
+const REPRODUCTION_RATE_HERBIVOROUS: u32 = 957;
+const CARNIVORE_EAT_PROBABILITY:u32 = 0;
 
 // TODO: use an entity shinny thing system component stuff.
 #[derive(Debug)]
@@ -49,7 +50,7 @@ impl Animal {
     fn update_position<R: Rng + ?Sized>(&mut self, dice: &mut R) {
         self.x = toric_coordinate(self.x, self.dx, SIZE_X);
         self.y = toric_coordinate(self.y, self.dy, SIZE_Y);
-        if PROBABILITY_MOVE > dice.gen_range(0..=100) {
+        if dice.gen_range(0..=100) >= PROBABILITY_MOVE  {
             // is this -1 neccessary?!
             self.dx = dice.gen_range(-1..=1);
             self.dy = dice.gen_range(-1..=1);
@@ -72,7 +73,17 @@ fn map_set(map: &mut [u8], x: usize, y: usize, val: u8) {
     map[pos] = val;
 }
 
-fn update_herbivous<R: Rng + ?Sized>(mut animals: Vec<Animal>, dice: &mut R) -> Vec<Animal> {
+fn reproduce_or_not<R: Rng + ?Sized>(x: usize, y: usize, dice: &mut R) -> Option<Animal> {
+    if dice.gen_range(0..1000) >= REPRODUCTION_RATE_HERBIVOROUS {
+        let dx = dice.gen_range(0..2);
+        let dy = dice.gen_range(0..2);
+        Some(Animal::new(x, y, dx, dy, ENERGY_DEFAULT))
+    } else {
+        None
+    }
+}
+
+fn update_animal<R: Rng + ?Sized>(mut animals: Vec<Animal>, dice: &mut R) -> Vec<Animal> {
     // update
     for a in animals.iter_mut() {
         a.update_position(dice);
@@ -82,15 +93,7 @@ fn update_herbivous<R: Rng + ?Sized>(mut animals: Vec<Animal>, dice: &mut R) -> 
     let new_animals = animals
         .iter()
         .map(|a| (a.x, a.y))
-        .filter_map(|(x, y)| {
-            if REPRODUCTION_RATE_HERBIVOROUS > dice.gen_range(0..1000) {
-                let dx = dice.gen_range(0..2);
-                let dy = dice.gen_range(0..2);
-                Some(Animal::new(x, y, dx, dy, ENERGY_DEFAULT))
-            } else {
-                None
-            }
-        })
+        .filter_map(|(x, y)| reproduce_or_not(x, y, dice))
         .collect::<Vec<_>>();
     animals.extend(new_animals);
     animals
@@ -98,11 +101,11 @@ fn update_herbivous<R: Rng + ?Sized>(mut animals: Vec<Animal>, dice: &mut R) -> 
 
 fn main() {
     let mut map = vec![b'.'; SIZE_X * SIZE_Y];
-    let mut poors_animals = Vec::with_capacity(32);
-    let mut predators = Vec::with_capacity(32);
+    let mut herbivors = Vec::with_capacity(32);
+    let mut carnivors = Vec::with_capacity(32);
     let mut rng = rand::thread_rng();
 
-    poors_animals.extend((0..NUMBER_OF_POOR_ANIMALS).map(|_| {
+    herbivors.extend((0..NUMBER_OF_POOR_ANIMALS).map(|_| {
         let x = rng.gen_range(0..SIZE_X);
         let y = rng.gen_range(0..SIZE_Y);
         let dx = rng.gen_range(0..2);
@@ -110,7 +113,7 @@ fn main() {
         Animal::new(x, y, dx, dy, ENERGY_DEFAULT)
     }));
 
-    predators.extend((0..NUMBER_OF_PREDATORS).map(|_| {
+    carnivors.extend((0..NUMBER_OF_PREDATORS).map(|_| {
         let x = rng.gen_range(0..SIZE_X);
         let y = rng.gen_range(0..SIZE_Y);
         let dx = rng.gen_range(0..2);
@@ -127,12 +130,12 @@ fn main() {
         }
 
         // print out "to-be-eaten" animals
-        for a in poors_animals.iter() {
+        for a in herbivors.iter() {
             map_set(&mut map, a.x, a.y, b'0');
         }
 
         // print out "hungry" animals
-        for a in predators.iter() {
+        for a in carnivors.iter() {
             map_set(&mut map, a.x, a.y, b'@');
         }
         {
@@ -149,6 +152,13 @@ fn main() {
         }
 
         // GAME UPDATE
-        poors_animals = update_herbivous(poors_animals, &mut rng);
+        herbivors = update_animal(herbivors, &mut rng);
+        carnivors = update_animal(carnivors, &mut rng);
+
+        // carnivors are hungry.
+        // Something is wront with this code.
+        herbivors.retain(|herbi| {
+            carnivors.iter().any(|carni| herbi.x != carni.x && herbi.y != herbi.y && !(rng.gen_range(0..=100) >= CARNIVORE_EAT_PROBABILITY))
+        });
     }
 }
